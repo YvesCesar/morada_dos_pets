dc83b26555c71914b1dcb956d8b0bf60ee8e0dae
YvesCesar <yvesamorim73@gmail.com>
2024-10-28 19:40:22 -0400

    Create about us initial page

---
+++ app/controllers/about_us_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AboutUsController < ApplicationController
+    def index
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "about_us", to: "about_us#index"
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
