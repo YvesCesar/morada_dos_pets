4de03f498ab5be7de5d9e06d11a525a00e922ae9
YvesCesar <yvesamorim73@gmail.com>
2024-11-01 20:09:03 -0400

    Carousel: initial template with SwiperJS

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -15,3 +15,16 @@ document.addEventListener('turbo:load', () => {
 
 
 export { application }
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "aos", to: "https://ga.jspm.io/npm:aos@3.0.0-beta.6/dist/aos.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "swiper" # @11.1.14
