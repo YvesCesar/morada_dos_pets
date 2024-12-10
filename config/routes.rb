373d3176f9a9082fe071822c2bf5dd36b19da725
YvesCesar <yvesamorim73@gmail.com>
2024-12-10 19:23:28 -0400

    Initial page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about_us", to: "about_us#index"
   get "services", to: "services#index"
+  scope "/services" do
+    get "dog_spa", to: "dog_spa#index"
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
